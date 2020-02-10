c0e5ecdaddab02000239e9adc6f61c7787c3b2da
my.suhadi <my.suhadi@gmail.com>
2020-02-11 06:08:16 +0700

    update

--- bagrenproject/sbu/views.py
+++ bagrenproject/sbu/views.py
@@ -7,29 +7,31 @@ from .forms import sbuForm
 
 
 def index(request):
-	daftar_sbu = sbu.objects.all()
+    daftar_sbu = sbu.objects.all()
 
-	konteks = {
-		'judul':'home sbu',
-		'daftar_sbu_':daftar_sbu,
-	}
+    konteks = {
+        'judul': 'home sbu',
+        'daftar_sbu_': daftar_sbu,
+    }
 
-	return render(request, 'sbu/index.html', konteks)
+    return render(request, 'sbu/index.html', konteks)
+
 
 def tambah_sbu(request):
-	sbu_form = sbuForm(request.POST or None)
-	if request.method == 'POST':
-		if sbu_form.is_valid():
-			sbu_form.save()
-		return redirect('sbu:index')
+    sbu_form = sbuForm(request.POST or None)
+    if request.method == 'POST':
+        if sbu_form.is_valid():
+            sbu_form.save()
+        return redirect('sbu:index')
 
-	konteks = {
-		'judul':'tambah sbu',
-		'sbu_form_': sbu_form,
-	}
+    konteks = {
+        'judul': 'tambah sbu',
+        'sbu_form_': sbu_form,
+    }
 
-	return render(request, 'sbu/tambah.html', konteks)
+    return render(request, 'sbu/tambah.html', konteks)
+
 
 def hapus_sbu(request, delete_id):
-	sbu.objects.filter(id=delete_id).delete()
-	return redirect('sbu:index')
+    sbu.objects.filter(id=delete_id).delete()
+    return redirect('sbu:index')
