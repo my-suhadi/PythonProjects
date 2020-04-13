891e4169898ebbbdb2a09844185c4c08b90132bc
my.suhadi <my.suhadi@gmail.com>
2020-04-14 00:36:53 +0700

    update #54

--- mywebsite/mywebsite/views.py
+++ mywebsite/mywebsite/views.py
@@ -1,26 +1,43 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 
 
 # === index view menggunakan class based view ===
 class IndexView(TemplateView):
-    template_name = 'index.html'
+    template_name = 'index_anonymous.html'
 
 
 # === index view menggunakan function based view ===
+# view method dengan internal permission check
 def index(request):
     konteks = {
         'judul_halaman': 'Beranda',
     }
 
-    return render(request, 'index.html', konteks)
+    print(request.user.is_authenticated)
+
+    # === blok internal permission check ===
+    template_name = None
+    if request.user.is_authenticated:
+        template_name = 'index_user.html'
+    else:
+        template_name = 'index_anonymous.html'
+
+    return render(request, template_name, konteks)
 
 
 def loginView(request):
     konteks = {
         'judul_halaman': 'LOGIN',
     }
+
+    if request.method == 'GET':
+        if request.user.is_authenticated:
+            return redirect('index')
+        else:
+            return render(request, 'login.html', konteks)
 
     if request.method == 'POST':
         print(request.POST)
@@ -37,9 +54,9 @@ def loginView(request):
         else:
             return redirect('masuk')
 
-    return render(request, 'login.html', konteks)
 
-
+# menggunakan dekorator utk masking login
+@login_required
 def logoutView(request):
     konteks = {
         'judul_halaman': 'Logout',
