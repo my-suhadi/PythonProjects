f48b48948bf065238ccfe048242002af288d8694
my.suhadi <my.suhadi@gmail.com>
2020-10-01 19:01:15 +0700

    initial commit

--- mysite/akun/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AkunConfig(AppConfig):
-    name = 'akun'
--- mysite/personal/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from personal.models import Pertanyaan
-
-admin.site.register(Pertanyaan)
