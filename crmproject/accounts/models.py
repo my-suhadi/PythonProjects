339310836f12e43370900710fafcfe2e3d56e8cd
suhadi <my.suhadi@gmail.com>
2020-10-08 20:23:05 +0700

    initial commit - part 7 complete

--- crmproject/accounts/models.py
+++ crmproject/accounts/models.py
@@ -13,6 +13,13 @@ class Customer(models.Model):
         return self.name
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     CATEGORY = (
         ('indoor', 'Indoor'),
@@ -22,16 +29,21 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     price = models.FloatField()
     category = models.CharField(max_length=200, choices=CATEGORY)
-    description = models.CharField(max_length=200)
+    description = models.CharField(max_length=200, blank=True)
     date_created = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
 
 
 class Order(models.Model):
     STATUS = (
         ('pending', 'Pending'),
         ('out of delivery', 'Out of delivery'),
-        ('delivered', 'Devlivered'),
+        ('delivered', 'Delivered'),
     )
-
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=200, choices=STATUS)
