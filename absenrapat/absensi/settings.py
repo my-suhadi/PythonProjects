11966e17f65354c069fb4ace0fe7f20b5cd13f18
suhadi <my.suhadi@gmail.com>
2020-11-26 16:47:11 +0700

    absenrapat - update template

--- absenrapat/absensi/settings.py
+++ absenrapat/absensi/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'agenda_rapat.apps.AgendaRapatConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- absenrapat/agenda_rapat/views.py
+++ absenrapat/agenda_rapat/views.py
@@ -2,6 +2,15 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from agenda_rapat.models import AgendaRapat
+
 
 def index(request):
-    return render(request, 'agenda_rapat/index.html', {})
+    all_agenda = AgendaRapat.objects.all()
+
+    cx = {
+        'judulKey': 'Judul',
+        'allAgendaKey': all_agenda,
+    }
+
+    return render(request, 'agenda_rapat/index.html', cx)
